3c8c720f880723f9b9a8f51c1b4f5f2fa90307a7
Kenny Allen <kenny.allen996@gmail.com>
2023-07-05 11:19:17 +0100

    Forgot to take this out of the dev enviroment, done now

--- listingOverride/run.js
+++ listingOverride/run.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const common = require('../common.js');
 const csv = require('fast-csv');
 
-global.enviroment = 'api.dev.stok.ly';
+global.enviroment = 'api.stok.ly';
 global.waitForGets = 1;
 
 async function getInput(){
@@ -27,9 +27,9 @@ async function getInput(){
 
 async function generateCSV(channelID){
     let csvArr = []
-    await common.loopThrough('Getting channel and item Data', `https://api.dev.stok.ly/v0/channels/${channelID}/listings`, 'size=1000', '[status]!={2}', async (listing)=>{
-        let listingData = await common.requester('get', `https://api.dev.stok.ly/v0/listings/${listing.listingId}`).then(r=>{return r.data.data})
-        let itemData = await common.requester('get', `https://api.dev.stok.ly/v0/items/${listing.itemId}`).then(r=>{return r.data.data})
+    await common.loopThrough('Getting channel and item Data', `https://${enviroment}/v0/channels/${channelID}/listings`, 'size=1000', '[status]!={2}', async (listing)=>{
+        let listingData = await common.requester('get', `https://${enviroment}/v0/listings/${listing.listingId}`).then(r=>{return r.data.data})
+        let itemData = await common.requester('get', `https://${enviroment}/v0/items/${listing.itemId}`).then(r=>{return r.data.data})
         csvArr.push({listing:listingData,item:itemData})
 
     })
@@ -54,17 +54,17 @@ async function generateCSV(channelID){
     let removeAll = await common.askQuestion('Remove all overrides = 1, or select from CSV = 0: ').then(r=>{return JSON.parse(r)})
 
     let attributeDict = {}
-    await common.loopThrough('Getting attribute lookup', `https://api.dev.stok.ly/v0/item-attributes`, 'size=1000', '[status]!={1}', (att)=>{
+    await common.loopThrough('Getting attribute lookup', `https://${enviroment}/v0/item-attributes`, 'size=1000', '[status]!={1}', (att)=>{
         attributeDict[att.itemAttributeId] = att.name.toLowerCase()
     })
 
-    await common.loopThrough('Updating Listings', `https://api.dev.stok.ly/v0/channels/${channelID}/listings`, 'size=1000', '[status]!={2}', async (listing)=>{  
+    await common.loopThrough('Updating Listings', `https://${enviroment}/v0/channels/${channelID}/listings`, 'size=1000', '[status]!={2}', async (listing)=>{  
         let patchData = {channelSpecifics:[]}
-        let listingData = await common.requester('get', `https://api.dev.stok.ly/v0/listings/${listing.listingId}`).then(r=>{return r.data.data.data})
+        let listingData = await common.requester('get', `https://${enviroment}/v0/listings/${listing.listingId}`).then(r=>{return r.data.data.data})
         if(listingData.listIndividually != undefined){patchData.listIndividually = listingData.listIndividually}
 
         if(removeAll){
-            await common.requester('patch', `https://api.dev.stok.ly/v0/listings/${listing.listingId}`, {data:patchData})
+            await common.requester('patch', `https://${enviroment}/v0/listings/${listing.listingId}`, {data:patchData})
             return
         }
 
@@ -80,7 +80,7 @@ async function generateCSV(channelID){
                 }
             }
         }
-        await common.requester('patch', `https://api.dev.stok.ly/v0/listings/${listing.listingId}`, {data:patchData})
+        await common.requester('patch', `https://${enviroment}/v0/listings/${listing.listingId}`, {data:patchData})
     })
 
 })()
