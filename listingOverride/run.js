587137910b4dda5f09f5a80533ff0f8c226eb5d5
Kenny Allen <kenny.allen996@gmail.com>
2023-07-05 11:52:27 +0100

    The user can now designate items they do not wish to override

--- listingOverride/run.js
+++ listingOverride/run.js
@@ -8,28 +8,29 @@ global.waitForGets = 1;
 
 async function getInput(){
     return new Promise((res,rej)=>{
-        let returnArr = []
+        let returnObj = {attsToRemove:[],itemsToSkip:[]}
         const stream = fs.createReadStream('./include.csv')
         .pipe(csv.parse({ headers: true }))
         .on('error', error => console.error(error))
         .on('data',  row => {
             stream.pause()
 
-                returnArr.push(row.Attribute.toLowerCase())
+            if(row.Attribute != ''){returnObj.attsToRemove.push(row.Attribute.toLowerCase())}
+            if(row.Items != ''){returnObj.itemsToSkip.push(row.Items.toLowerCase())}
 
             stream.resume()
         })
         .on('end', () => {
-            res(returnArr)
+            res(returnObj)
         })
     })
 }
 
 async function generateCSV(channelID){
     let csvArr = []
-    await common.loopThrough('Getting channel and item Data', `https://${enviroment}/v0/channels/${channelID}/listings`, 'size=1000', '[status]!={2}', async (listing)=>{
-        let listingData = await common.requester('get', `https://${enviroment}/v0/listings/${listing.listingId}`).then(r=>{return r.data.data})
-        let itemData = await common.requester('get', `https://${enviroment}/v0/items/${listing.itemId}`).then(r=>{return r.data.data})
+    await common.loopThrough('Getting channel and item Data', `https://${global.enviroment}/v0/channels/${channelID}/listings`, 'size=1000', '[status]!={2}', async (listing)=>{
+        let listingData = await common.requester('get', `https://${global.enviroment}/v0/listings/${listing.listingId}`).then(r=>{return r.data.data})
+        let itemData = await common.requester('get', `https://${global.enviroment}/v0/items/${listing.itemId}`).then(r=>{return r.data.data})
         csvArr.push({listing:listingData,item:itemData})
 
     })
@@ -39,7 +40,7 @@ async function generateCSV(channelID){
 
 (async ()=>{
 
-    let removeAtts = await getInput()
+    let inputVals = await getInput()
 
     let channelID = await common.askQuestion('Enter the channel ID: ')
 
@@ -49,37 +50,45 @@ async function generateCSV(channelID){
 
     if(getCSV){
         await generateCSV(channelID)
-        let continueScript = await common.askQuestion('CSV Generated. Press ENTER to continue: ').then(r=>{return r.toLowerCase()})
+        await common.askQuestion('CSV Generated. Press ENTER to continue: ').then(r=>{return r.toLowerCase()})
+    }
+
+    if (inputVals.itemsToSkip.length > 0){
+        var itemDict = {}
+        await common.loopThrough('Getting Item Lookup', `https://${global.enviroment}/v0/items`, 'size=1000', '[status]!={1}', (item)=>{
+            itemDict[item.itemId] = item.sku.toLowerCase()
+        })
     }
 
     let attributeDict = {}
-    await common.loopThrough('Getting attribute lookup', `https://${enviroment}/v0/item-attributes`, 'size=1000', '[status]!={1}', (att)=>{
+    await common.loopThrough('Getting attribute lookup', `https://${global.enviroment}/v0/item-attributes`, 'size=1000', '[status]!={1}', (att)=>{
         attributeDict[att.itemAttributeId] = att.name.toLowerCase()
     })
 
-    await common.loopThrough('Updating Listings', `https://${enviroment}/v0/channels/${channelID}/listings`, 'size=1000', '[status]!={2}', async (listing)=>{  
+    await common.loopThrough('Updating Listings', `https://${global.enviroment}/v0/channels/${channelID}/listings`, 'size=1000', '[status]!={2}', async (listing)=>{
+        if(itemDict && inputVals.itemsToSkip.includes(itemDict[listing.itemId])){return}
         let patchData = {channelSpecifics:[]}
-        let listingData = await common.requester('get', `https://${enviroment}/v0/listings/${listing.listingId}`).then(r=>{return r.data.data.data})
+        let listingData = await common.requester('get', `https://${global.enviroment}/v0/listings/${listing.listingId}`).then(r=>{return r.data.data.data})
         if(listingData.listIndividually != undefined){patchData.listIndividually = listingData.listIndividually}
 
         if(removeAll){
-            await common.requester('patch', `https://${enviroment}/v0/listings/${listing.listingId}`, {data:patchData})
+            await common.requester('patch', `https://${global.enviroment}/v0/listings/${listing.listingId}`, {data:patchData})
             return
         }
 
         for(const property of Object.keys(listingData)){
-            if(['itemId','attributes','stokly_type','listIndividually','variableItemId','variantListingIds'].includes(property) || removeAtts.includes(property.toLowerCase())){continue}
+            if(['itemId','attributes','stokly_type','listIndividually','variableItemId','variantListingIds'].includes(property) || inputVals.attsToRemove.includes(property.toLowerCase())){continue}
             patchData[property] = listingData[property]
         }
         if(listingData.attributes){
             patchData.attributes = []
             for (const attribute of listingData.attributes){
-                if(!removeAtts.includes(attributeDict[attribute.attributeId])){
+                if(!inputVals.attsToRemove.includes(attributeDict[attribute.attributeId])){
                     patchData.attributes.push(attribute) 
                 }
             }
         }
-        await common.requester('patch', `https://${enviroment}/v0/listings/${listing.listingId}`, {data:patchData})
+        await common.requester('patch', `https://${global.enviroment}/v0/listings/${listing.listingId}`, {data:patchData})
     })
 
 })()
