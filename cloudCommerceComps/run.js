eb45ef4b0deb6953c83710a7011c54c7170f1780
Kenny <kenny.allen996@gmail.com>
2023-07-12 17:25:57 +0100

    cloud commerce comps script made

---
+++ cloudCommerceComps/run.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const fastCSV = require('fast-csv');
+const myWrite = fs.createWriteStream('./output.csv',{flags:'a'});
+
+fs.writeFileSync('./output.csv', '"sku","composing sku","quantity in item"\r\n')
+
+const stream = fs.createReadStream('./input.csv')
+.pipe(fastCSV.parse({ headers: true }))
+.on('error', error => console.error(error))
+.on('data',  async row => {
+    stream.pause()
+
+    let childCount = 1
+
+    do{
+
+        myWrite.write(`"${row.PROD_SKU}",`)
+        myWrite.write(`"${row[`ITEM${childCount}_SKULinks`]}",`)
+        myWrite.write(`"${row[`ITEM${childCount}_Quantity`]}",`)
+        myWrite.write(`\r\n`)
+
+        childCount += 1
+
+    } while ((row[`ITEM${childCount}_SKULinks`] || '') != '')
+
+    stream.resume()
+})
