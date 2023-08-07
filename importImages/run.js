1730c4b0f6e6dda4b3370050138b9e4e60777408
Kenny Allen <kenny.allen996@gmail.com>
2023-08-07 13:59:26 +0100

    more stuff

--- importImages/run.js
+++ importImages/run.js
@@ -1,12 +1,17 @@
 const compress_images = require("compress-images");
-let fs = require("fs");
+const fs = require("fs");
+const common = require('../common.js');
+
+global.enviroment = 'api.stok.ly'
 
 compAmount = 70
 
-async function compressImgs(fileIn) {
+let itemDict = {}
+
+async function compressImgs(fileIn, fileOut) {
     return new Promise((resolve, reject) => {
         compress_images(
-            fileIn, `./compressedImages/`,
+            fileIn, fileOut,
             { compress_force: false, statistic: true, autoupdate: true },
             false,
             { jpg: { engine: "mozjpeg", command: ["-quality", compAmount] } },
@@ -33,11 +38,42 @@ function readdir(directory) {
     })
 }
 
-async function moveImages(source, destination){
+async function getImages(source){
+    let dir = await readdir(source)
 
+    for (const file of dir){
+        let fileStat = fs.statSync(`./${source}/${file}`)
+        if(fileStat.isDirectory()){
+            await getImages(`./${source}/${file}`)
+        } else {
+            if((fileStat.size * 0.000001) > 2){
+                let result = await compressImgs(`./${source}/${file}`, `./${source}/compressed - `)
+            }
+        }
+    }
 }
 
 (async ()=>{
+
+    try{
+        fs.statSync('./inputFolder')
+    } catch {
+        fs.mkdirSync('./inputFolder');
+    }
+
+    await common.askQuestion(`Move images into 'inputFolder'. If this did not already exist, it has been created. Press ENTER to continue:`)
+
+    let matchProperty = await common.askQuestion(`How are we looking up the items? 1 = SKU, 0 = Barcode: `).then(r=>{return JSON.parse(r)})
+
+    // await common.loopThrough('Getting Items', `https://${global.enviroment}/v0/items`, '', `[status]!={1}`, async (item)=>{
+    //     if(!matchProperty){
+    //         if(item.barcodes != undefined){itemDict[item.barcode] = {itemId:item.itemId}}
+    //     } else {
+    //         itemDict[item.sku] = {itemId:item.itemId}
+    //     }
+    // })
+
+    await getImages('./inputFolder')
 
     // let k = await readdir('./compressedImages')
 
@@ -50,6 +86,6 @@ async function moveImages(source, destination){
 
     // console.log(b.size * 0.000001)
     
-    let i = await compressImgs('./example.JPG')
-    console.log(i)
+    // let i = await compressImgs('./example.JPG')
+    // console.log(i)
 })()
