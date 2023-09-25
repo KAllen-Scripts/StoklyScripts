825995bdef4d031f45e19c29f716040918f825e3
Kenny Allen <kenny.allen996@gmail.com>
2023-09-25 17:10:20 +0100

    Fuck you this is good enough

---
+++ addBins/run.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const csv = require('fast-csv');
+const common = require('../common.js')
+
+global.enviroment = 'api.stok.ly'
+
+async function getInput(){
+    let rowDone = false
+    return new Promise((res,rej)=>{
+        let payload = {bins:[]}
+        const stream = fs.createReadStream('./input.csv')
+        .pipe(csv.parse({ headers: true }))
+        .on('error', error => console.error(error))
+        .on('data',  row => {
+            stream.pause()
+
+                rowDone = false
+
+                payload.bins.push({
+                    "name": row.bin,
+                    "type": row.type,
+                    "barcode": row.barcode
+                })
+
+                rowDone = true
+
+            stream.resume()
+        })
+        .on('end', async () => {
+            do{
+                await common.sleep(200)
+            } while (!rowDone)
+            res(payload)
+        })
+    })
+}
+
+(async ()=>{
+    let locationID = await common.askQuestion('Enter the location ID: ')
+
+    let payload = await getInput()
+
+    await common.requester('patch', `https://${global.enviroment}/v0/locations/${locationID}`, payload)
+
+})()   
