dbd994e5585bac978c0f9947942b150d08145c77
Kenny Allen <kenny.allen996@gmail.com>
2023-11-06 13:02:07 +0000

    Can now define orders to skip Can now also adjust stock back so no fake location needs to be made

--- collectItems/run.js
+++ collectItems/run.js
@@ -6,19 +6,20 @@ global.enviroment = 'api.stok.ly'
 
 async function getInput(){
     return new Promise((res,rej)=>{
-        let returnArr = []
+        let returnObj = {optIn:[], optOut:[]}
         const stream = fs.createReadStream('./include.csv')
-        .pipe(csv.parse({ headers: true }))
+        .pipe(csv.parse({headers: headers => headers.map(h => h.toLowerCase())}))
         .on('error', error => console.error(error))
         .on('data',  row => {
             stream.pause()
 
-                returnArr.push(parseInt(row.niceID))
+                returnObj.optIn.push(parseInt(row.clear))
+                returnObj.optOut.push(parseInt(row["don't clear"]))
 
             stream.resume()
         })
         .on('end', () => {
-            res(returnArr)
+            res(returnObj)
         })
     })
 }
@@ -27,32 +28,55 @@ async function getInput(){
 
     const locationID = await common.askQuestion(`What is the ID of the location we are collecting from?: `)
     const clearBefore = await common.askQuestion(`What order are we clearing up to?: `)
+    const resetInv = await common.askQuestion(`Are we undoing any adjustments this will cause? 1 = Yes, 0 = No: `).then(r=>{return parseInt(r)})
 
-    let optInArr = await getInput()
+    let options = await getInput()
+    let optInArr = options.optIn
+    let optOutArr = options.optOut
+
+    console.log(optOutArr)
 
     let toCollect = []
 
-    await common.loopThrough('Checking Orders', `https://${global.enviroment}/v2/saleorders`, `sortDirection=ASC&sortField=createdAt&size=1000`, `(([stage]=={order}%26%26[itemStatuses]::{unprocessed}))%26%26([stage]!={removed})`, async (item)=>{
+    const defaultBin = await common.requester('get', `https://${global.enviroment}/v0/locations/${locationID}/bins?filter=[default]=={1}`).then(r=>{return r.data.data[0].binId})
+
+    await common.loopThrough('Checking Orders', `https://${global.enviroment}/v2/saleorders`, `sortDirection=DESC&sortField=createdAt&size=1000`, `(([stage]=={order}%26%26[itemStatuses]::{unprocessed}))%26%26([stage]!={removed})`, async (item)=>{
+        if(optOutArr.includes(item.niceId)){return}
         if(optInArr.includes(item.niceId) || parseInt(item.niceId) <= clearBefore){toCollect.push(item)}
     })
 
     for (const item in toCollect){
-        await common.requester('get',`https://${global.enviroment}/v2/saleorders/${toCollect[item].saleOrderId}/items`).then(async r=>{
+        await common.requester('get',`https://${global.enviroment}/v2/saleorders/${toCollect[item].saleOrderId}/items?size=1000`).then(async r=>{
 
             let postObj = {
                 items:[],
                 locationId: locationID
             }
 
+            let adjustObj = {
+                items: [],
+                locationId: locationID,
+                binId: defaultBin,
+                reason: 'Clearing Orders'
+            }
+
             for(const i of r.data.data){
+                if(i.status != 'unprocessed'){continue}
+
+                adjustObj.items.push({
+                    itemId: i.referenceId,
+                    quantity: i.quantity
+                })
+
                 postObj.items.push({
                     lineId: i.lineId,
                     quantity: i.quantity
                 })
             }
             await common.requester('post',`https://${global.enviroment}/v2/saleorders/${toCollect[item].saleOrderId}/collect-items`, postObj)
+            if(resetInv){await common.requester('post', `https://${global.enviroment}/v1/adjustments`, adjustObj)}
             await common.sleep(3000)
-            console.log(`Done ${parseInt(item) + 1} of ${toCollect.length}`)
+            console.log(`Done ${parseInt(item) + 1} of ${toCollect.length} | ${toCollect[item].niceId}`)
         })
         .catch(err=>{console.log(err)})
     }
