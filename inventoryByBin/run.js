380bee8fb5f946de39d6a725153b0da92e25599c
Kenny Allen <kenny.allen996@gmail.com>
2024-01-23 12:01:11 +0000

    Can now export inventory by bins

---
+++ inventoryByBin/run.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+const common = require('../common.js');
+const ExcelJS = require('exceljs');
+
+global.enviroment = 'api.stok.ly';
+
+async function createSheet(workbook, sheetName, data) {
+    const worksheet = workbook.addWorksheet(sheetName);
+
+    let rows = await convertToArrays(data);
+
+    worksheet.addRows(rows);
+}
+
+(async ()=>{
+
+    const workbook = new ExcelJS.Workbook();
+
+    let locations = {}
+    await common.loopThrough('Getting Bins', `https://${global.enviroment}/v1/inventory-records`, 'size=1000', `[binId]!={UNASSIGNED}`, (record)=>{
+        if (!locations[record.locationName]){locations[record.locationName] = {}}
+        if (!locations[record.locationName][record.itemSku]){locations[record.locationName][record.itemSku] = {
+            itemId: record.itemId,
+            name: record.itemName,
+            bins: {}
+        }}
+
+        locations[record.locationName][record.itemSku].bins[`${record.binName} on Hand`] = record.onHand      
+    })
+
+    for (const location in locations){
+        await createSheet(workbook, location, locations[location])
+    }
+
+    workbook.xlsx.writeFile('./inventory.xlsx')
+        .then(() => {
+                console.log('Workbook created successfully!');
+        })
+        .catch((error) => {
+            console.error('Error creating workbook:', error);
+        });
+
+})()
+
+
+async function convertToArrays(data){
+    let headerArr = ['SKU', 'Name', 'itemId']
+    let rowArr = []
+
+    for(const item in data){
+        let row = [item, data[item].name, data[item].itemId]
+        for (const bin in data[item].bins){
+            if(!headerArr.includes(bin)){headerArr.push(bin)}
+            row[headerArr.indexOf(bin)] = data[item].bins[bin]
+        }
+
+        rowArr.push(row)
+    }
+
+    for (const row in rowArr){
+        for (const header in headerArr){
+            if (rowArr[row][header] == undefined){rowArr[row][header] = 0}
+        }
+    }
+
+    return [headerArr, ...rowArr]
+}
