120b8c5717eb853b616702d8549562c97747144a
Kenny Allen <kenny.allen996@gmail.com>
2024-02-02 15:17:56 +0000

    More fucking detail

--- getChildProducts/run.js
+++ getChildProducts/run.js
@@ -10,15 +10,25 @@ global.enviroment = 'api.stok.ly';
     let fileName = `./${(()=>{if (type > 2){return "Both"} else {return type == 1 ? "Composites" : "Variables"}})()}.csv`
 
     let myWrite = fs.createWriteStream(fileName, {flags:'a'})
-    fs.writeFileSync(fileName,'"parent SKU","parent name","Format","child sku","child Name","Composing Quantity"\r\n')
+    fs.writeFileSync(fileName,'"parent SKU","parent name","Variable Attributes","Format","child sku","child Name","Composing Quantity"\r\n')
 
     await common.loopThrough('Getting Items', `https://${global.enviroment}/v0/items`, 'size=1000', `([status]!={1})%26%26${type > 2 ? '(([format]!={0}))' : '(([format]=={' + type + '}))'}`, async (item)=>{
         if(item.format == type || type > 2){
 
+            let variableAttributes = await (async ()=>{
+                if(item.format == 2){
+                    let i = await common.requester('get', `https://api.stok.ly/v0/items/${item.itemId}/variable-attributes`).then(r=>{return r.data.data.map(item => item.name)})
+                    return i
+                } else {
+                    return ''
+                }
+            })()
+    
             let children = await common.requester('get', `https://${global.enviroment}/v0/items/${item.itemId}/children`).then(r=>{return r.data.data})
             if (children.length == 0){
                 myWrite.write(`"${item.sku}",`)
                 myWrite.write(`"${item.name}",`)
+                myWrite.write(`"${variableAttributes.join()}",`)
                 myWrite.write(`"${item.format == 1 ? 'Composite' : 'Variable'}",`)
                 myWrite.write('\r\n')
             }
@@ -26,6 +36,7 @@ global.enviroment = 'api.stok.ly';
             for (const child of children){
                 myWrite.write(`"${item.sku}",`)
                 myWrite.write(`"${item.name}",`)
+                myWrite.write(`"${variableAttributes.join()}",`)
                 myWrite.write(`"${item.format == 1 ? 'Composite' : 'Variable'}",`)
                 myWrite.write(`"${child.sku}",`)
                 myWrite.write(`"${child.name}",`)
